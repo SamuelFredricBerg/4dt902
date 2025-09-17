343c0f3b9bd2a6d215e8998b9681626d87317cf5
Samuel Fredric Berg <samuelberg.private@gmail.com>
2025-09-17 08:17:49 +0200

    Removed non needed impl file

--- a2/src/ofp/PrintListener.java
+++ a2/src/ofp/PrintListener.java
@@ -1,7 +1,22 @@
 package ofp;
 
 import generated.OFPBaseListener;
+import org.antlr.v4.runtime.ParserRuleContext;
 
 public class PrintListener extends OFPBaseListener {
+    /**
+     * {@inheritDoc}
+     *
+     * <p>
+     * The default implementation does nothing.
+     * </p>
+     */
+    @Override
+    public void enterEveryRule(ParserRuleContext ctx) {
+        printCurrent(ctx);
+    }
 
+    public void printCurrent(ParserRuleContext ctx) {
+        System.out.println("  ".repeat(ctx.depth()) + ctx.getClass().getSimpleName());
+    }
 }
