c617b786ccfa9e5e4461e02f4eba0511626c07c7
Samuel Fredric Berg <samuelberg.private@gmail.com>
2025-09-25 08:25:08 +0200

    Added somewhat repetitive JavaDoc to the PythonGenerator files

--- a4/src/ofp/PythonGenerator.java
+++ a4/src/ofp/PythonGenerator.java
@@ -9,11 +9,17 @@ import org.antlr.v4.runtime.tree.ParseTreeProperty;
 import generated.OFPBaseVisitor;
 import generated.OFPParser;
 
+/**
+ * Visitor that generates Python code from the OFP parse tree.
+ * Handles translation of OFP constructs to Python syntax, including indentation
+ * and reserved identifiers.
+ */
 public class PythonGenerator extends OFPBaseVisitor<String> {
     private int depth = 0;
     private ParseTreeProperty<Scope> scopes;
     private Scope currentScope;
 
+    /** Set of Python reserved identifiers to avoid naming conflicts. */
     private static HashSet<String> reservedIds = new HashSet<String>(Arrays.asList("False", "None", "True", "and", "as",
             "assert", "async", "await", "break", "class", "continue", "def", "del", "elif", "else", "except", "finally",
             "for", "from", "global", "if", "import", "in", "is", "lambda", "nonlocal", "not", "or", "pass", "raise",
@@ -43,6 +49,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         this.scopes = scopes;
     }
 
+    /**
+     * Generates Python code for the program.
+     * 
+     * @param ctx the program context
+     * @return the generated Python code
+     */
     @Override
     public String visitProgram(OFPParser.ProgramContext ctx) {
         StringBuilder progStmt = new StringBuilder();
@@ -58,6 +70,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return progStmt.toString();
     }
 
+    /**
+     * Generates Python code for the main function.
+     * 
+     * @param ctx the main context
+     * @return the generated Python code for main
+     */
     @Override
     public String visitMain(OFPParser.MainContext ctx) {
         int previousDepth = depth;
@@ -69,6 +87,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return mainStmt;
     }
 
+    /**
+     * Generates Python code for a function declaration.
+     * 
+     * @param ctx the function declaration context
+     * @return the generated Python code for the function
+     */
     @Override
     public String visitFuncDecl(OFPParser.FuncDeclContext ctx) {
         StringBuilder functionDeclStmt = new StringBuilder();
@@ -94,6 +118,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return functionDeclStmt.toString();
     }
 
+    /**
+     * Generates Python code for a function call.
+     * 
+     * @param ctx the function call context
+     * @return the generated Python code for the function call
+     */
     @Override
     public String visitFuncCall(OFPParser.FuncCallContext ctx) {
         StringBuilder functionCallStmt = new StringBuilder();
@@ -114,6 +144,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return functionCallStmt.toString();
     }
 
+    /**
+     * Generates Python code for a block of statements.
+     * 
+     * @param ctx the block context
+     * @return the generated Python code for the block
+     */
     @Override
     public String visitBlock(OFPParser.BlockContext ctx) {
         StringBuilder blockStmt = new StringBuilder();
@@ -134,6 +170,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return blockStmt.toString();
     }
 
+    /**
+     * Generates Python code for print and println statements.
+     * 
+     * @param ctx the print statement context
+     * @return the generated Python code for the print statement
+     */
     @Override
     public String visitPrintStmt(OFPParser.PrintStmtContext ctx) {
         StringBuilder printStmt = new StringBuilder();
@@ -161,11 +203,23 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return printStmt.toString();
     }
 
+    /**
+     * Generates Python code for a function call statement.
+     * 
+     * @param ctx the function call statement context
+     * @return the generated Python code for the statement
+     */
     @Override
     public String visitFuncCallStmt(OFPParser.FuncCallStmtContext ctx) {
         return indent() + visit(ctx.funcCall()) + "\n";
     }
 
+    /**
+     * Generates Python code for an assignment statement.
+     * 
+     * @param ctx the assignment statement context
+     * @return the generated Python code for the assignment
+     */
     @Override
     public String visitAssignStmt(OFPParser.AssignStmtContext ctx) {
         String varName = getSafeId(ctx.ID().getText());
@@ -180,6 +234,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         }
     }
 
+    /**
+     * Generates Python code for a variable declaration statement.
+     * 
+     * @param ctx the variable declaration statement context
+     * @return the generated Python code for the declaration
+     */
     @Override
     public String visitVarDeclStmt(OFPParser.VarDeclStmtContext ctx) {
         String varName = getSafeId(ctx.ID().getText());
@@ -188,6 +248,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return indent() + varName + " = " + expr + "\n";
     }
 
+    /**
+     * Generates Python code for an if statement (with optional elif/else).
+     * 
+     * @param ctx the if statement context
+     * @return the generated Python code for the if statement
+     */
     @Override
     public String visitIfStmt(OFPParser.IfStmtContext ctx) {
         StringBuilder ifStmt = new StringBuilder();
@@ -230,6 +296,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return ifStmt.toString();
     }
 
+    /**
+     * Generates Python code for a while statement.
+     * 
+     * @param ctx the while statement context
+     * @return the generated Python code for the while statement
+     */
     @Override
     public String visitWhileStmt(OFPParser.WhileStmtContext ctx) {
         StringBuilder whileStmt = new StringBuilder();
@@ -244,6 +316,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return whileStmt.toString();
     }
 
+    /**
+     * Generates Python code for a return statement.
+     * 
+     * @param ctx the return statement context
+     * @return the generated Python code for the return statement
+     */
     @Override
     public String visitReturnStmt(OFPParser.ReturnStmtContext ctx) {
         StringBuilder returnStmt = new StringBuilder();
@@ -255,6 +333,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return returnStmt.toString();
     }
 
+    /**
+     * Generates Python code for array initialization expressions.
+     * 
+     * @param ctx the array initialization expression context
+     * @return the generated Python code for the array initialization
+     */
     @Override
     public String visitArrayInitExpr(OFPParser.ArrayInitExprContext ctx) {
         StringBuilder arrayInit = new StringBuilder();
@@ -276,6 +360,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return arrayInit.toString();
     }
 
+    /**
+     * Generates Python code for array access expressions.
+     * 
+     * @param ctx the array access expression context
+     * @return the generated Python code for the array access
+     */
     @Override
     public String visitArrayAccessExpr(OFPParser.ArrayAccessExprContext ctx) {
         String varName = getSafeId(ctx.ID().getText());
@@ -283,6 +373,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return varName + "[" + visit(ctx.expr()) + "]";
     }
 
+    /**
+     * Generates Python code for array length expressions.
+     * 
+     * @param ctx the array length expression context
+     * @return the generated Python code for the length expression
+     */
     @Override
     public String visitArrayLengthExpr(OFPParser.ArrayLengthExprContext ctx) {
         StringBuilder lengthExpr = new StringBuilder();
@@ -296,6 +392,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return lengthExpr.toString();
     }
 
+    /**
+     * Generates Python code for parenthesized expressions.
+     * 
+     * @param ctx the parenthesized expression context
+     * @return the generated Python code for the expression
+     */
     @Override
     public String visitParenExpr(OFPParser.ParenExprContext ctx) {
         StringBuilder parenthesesExpr = new StringBuilder();
@@ -307,6 +409,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return parenthesesExpr.toString();
     }
 
+    /**
+     * Generates Python code for unary expressions.
+     * 
+     * @param ctx the unary expression context
+     * @return the generated Python code for the unary expression
+     */
     @Override
     public String visitUnaryExpr(OFPParser.UnaryExprContext ctx) {
         StringBuilder unaryExpr = new StringBuilder();
@@ -318,6 +426,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return unaryExpr.toString();
     }
 
+    /**
+     * Generates Python code for multiplication/division expressions.
+     * 
+     * @param ctx the multiplication/division expression context
+     * @return the generated Python code for the expression
+     */
     @Override
     public String visitMultExpr(OFPParser.MultExprContext ctx) {
         StringBuilder multExpr = new StringBuilder();
@@ -329,6 +443,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return multExpr.toString();
     }
 
+    /**
+     * Generates Python code for addition/subtraction expressions.
+     * 
+     * @param ctx the addition/subtraction expression context
+     * @return the generated Python code for the expression
+     */
     @Override
     public String visitAddiExpr(OFPParser.AddiExprContext ctx) {
         StringBuilder addiExpr = new StringBuilder();
@@ -342,6 +462,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return addiExpr.toString();
     }
 
+    /**
+     * Generates Python code for relational expressions.
+     * 
+     * @param ctx the relational expression context
+     * @return the generated Python code for the expression
+     */
     @Override
     public String visitRelExpr(OFPParser.RelExprContext ctx) {
         StringBuilder relationalExpr = new StringBuilder();
@@ -355,6 +481,12 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return relationalExpr.toString();
     }
 
+    /**
+     * Generates Python code for equality expressions.
+     * 
+     * @param ctx the equality expression context
+     * @return the generated Python code for the expression
+     */
     @Override
     public String visitEqExpr(OFPParser.EqExprContext ctx) {
         StringBuilder equalityExpr = new StringBuilder();
@@ -368,16 +500,34 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         return equalityExpr.toString();
     }
 
+    /**
+     * Returns the Python code for an integer literal.
+     * 
+     * @param ctx the integer expression context
+     * @return the integer literal as a string
+     */
     @Override
     public String visitIntExpr(OFPParser.IntExprContext ctx) {
         return ctx.INT().getText();
     }
 
+    /**
+     * Returns the Python code for a float literal.
+     * 
+     * @param ctx the float expression context
+     * @return the float literal as a string
+     */
     @Override
     public String visitFloatExpr(OFPParser.FloatExprContext ctx) {
         return ctx.FLOAT().getText();
     }
 
+    /**
+     * Returns the Python code for a boolean literal.
+     * 
+     * @param ctx the boolean expression context
+     * @return the boolean literal as a string
+     */
     @Override
     public String visitBoolExpr(OFPParser.BoolExprContext ctx) {
         if ("true".equals(ctx.BOOLEAN().getText())) {
@@ -387,25 +537,55 @@ public class PythonGenerator extends OFPBaseVisitor<String> {
         }
     }
 
+    /**
+     * Returns the Python code for a char literal.
+     * 
+     * @param ctx the char expression context
+     * @return the char literal as a string
+     */
     @Override
     public String visitCharExpr(OFPParser.CharExprContext ctx) {
         return ctx.CHAR().getText();
     }
 
+    /**
+     * Returns the Python code for a string literal.
+     * 
+     * @param ctx the string expression context
+     * @return the string literal as a string
+     */
     @Override
     public String visitStringExpr(OFPParser.StringExprContext ctx) {
         return ctx.STRING().getText();
     }
 
+    /**
+     * Returns the Python code for a variable reference, handling reserved
+     * identifiers.
+     * 
+     * @param ctx the ID expression context
+     * @return the variable name as a string
+     */
     @Override
     public String visitIDExpr(OFPParser.IDExprContext ctx) {
         return getSafeId(ctx.ID().getText());
     }
 
+    /**
+     * Returns the current indentation string based on depth.
+     * 
+     * @return the indentation string
+     */
     private String indent() {
         return " ".repeat(depth * 2);
     }
 
+    /**
+     * Returns a safe identifier for Python, prefixing reserved words.
+     * 
+     * @param id the identifier to check
+     * @return a safe identifier string
+     */
     private static String getSafeId(String id) {
         if (reservedIds.contains(id)) {
             return "ofp_" + id;
