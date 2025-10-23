f0d02cddc7ec99874cd25a2eeb6d3a9bf9a2c6a7
Samuel Fredric Berg <samuelberg.private@gmail.com>
2025-10-23 08:09:13 +0200

    Removed dupe zip file

--- a4/src/ofp/Main.java
+++ a4/src/ofp/Main.java
@@ -27,7 +27,7 @@ public class Main extends ClassLoader implements Opcodes {
     public static void main(String[] args) {
         // Select test program
         String inputDir = "/home/fred/Documents/4dt902/input/";
-        String testFile = "test2"; // Change test file here
+        String testFile = "max"; // Change test file here
         String testProgram = testFile + ".ofp";
         String outputDir = "/home/fred/Documents/4dt902/a4/src/output/";
 
