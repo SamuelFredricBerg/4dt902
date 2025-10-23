5bd3de80fb0df1312794e0a48e6877365cf62b47
Samuel Fredric Berg <samuelberg.private@gmail.com>
2025-10-23 08:12:11 +0200

    Testing

--- a4/src/ofp/Main.java
+++ a4/src/ofp/Main.java
@@ -26,7 +26,7 @@ import generated.OFPParser;
 public class Main extends ClassLoader implements Opcodes {
     public static void main(String[] args) {
         // Select test program
-        String inputDir = "/home/fred/Documents/4dt902/input/";
+        String inputDir = "/home/fred/Documents/4dt902/input/a4/";
         String testFile = "max"; // Change test file here
         String testProgram = testFile + ".ofp";
         String outputDir = "/home/fred/Documents/4dt902/a4/src/output/";
